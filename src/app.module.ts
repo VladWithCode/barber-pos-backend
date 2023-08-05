c17371cd481473403954deedb1d944b812843d76
vladwithcode <vladwithcode@gmail.com>
2023-08-04 21:52:13 -0600

    [Change] Uncomment Product module import

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './products/products.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     MongooseModule.forRoot(process.env.DB_CONN_STRING),
-    // ProductsModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
