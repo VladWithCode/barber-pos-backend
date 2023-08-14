f07b12451a29a44d2fd196d56c4b1fd94d39eb8e
vladwithcode <vladwithcode@gmail.com>
2023-08-14 09:49:39 -0600

    Add DB & File upload dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { MulterModule } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_CONN_STRING),
+    MulterModule.register({
+      storage: memoryStorage(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
