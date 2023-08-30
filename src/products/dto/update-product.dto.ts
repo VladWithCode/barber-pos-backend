a5b70edb516121b7413db2c272b826633467d894
vladwithcode <vladwithcode@gmail.com>
2023-08-30 15:43:47 -0600

    [WIP] Update DTO

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -2,3 +2,10 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateProductDto } from './create-product.dto';
 
 export class UpdateProductDto extends PartialType(CreateProductDto) {}
+
+export class UpdateStockDto {
+  buy_price: number;
+  sell_units: number;
+  supply_units: number;
+  
+}
