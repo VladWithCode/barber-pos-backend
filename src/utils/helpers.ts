7490c74084815f606e0da1ba1094532893624fcb
vladwithcode <vladwithcode@gmail.com>
2023-09-01 18:21:43 -0600

    Add number converter (No decimal places)

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -7,3 +7,14 @@ export async function asyncHandler<T>(
     return [e, undefined];
   }
 }
+
+export function numberToSafeAmount(n: number): number {
+  let safeN = 0;
+  const [i, d] = n.toFixed(2).split('.');
+
+  safeN += parseInt(d);
+
+  safeN += parseInt(i) * 100;
+
+  return safeN;
+}
