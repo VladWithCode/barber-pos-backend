366e0cee678e31b883b24e64580de678602689a9
vladwithcode <vladwithcode@gmail.com>
2023-09-12 18:28:04 -0600

    Fuck CORS

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,37 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpException, HttpStatus, ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
+
+const corsWhitelist = [
+  'http://localhost:3030',
+  'http://192.168.1.2:3000',
+  'https://barbershop-pos.vercel.app',
+];
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:3030',
-      'http://192.168.1.2:3030',
-      'https://barbershop-pos.vercel.app',
-    ],
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    origin: function (origin, callback) {
+      if (!origin) {
+        callback(null, true);
+        return;
+      }
+      if (
+        corsWhitelist.includes(origin) || // Checks your whitelist
+        !!origin.match(/api.certx-mx\.org$/) // Overall check for your domain
+      ) {
+        callback(null, true);
+      } else {
+        callback(
+          new HttpException('Not allowed by CORS', HttpStatus.I_AM_A_TEAPOT),
+          false,
+        );
+      }
+    },
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.useStaticAssets(join(process.cwd(), 'public'));
