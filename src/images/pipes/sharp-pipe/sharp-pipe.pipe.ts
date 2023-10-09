9908ca3c00c2070f1971bd5f4f8615256e72f870
vladwithcode <vladwithcode@gmail.com>
2023-10-09 14:04:33 -0600

    Add multiple file transform pipe

--- src/images/pipes/sharp-pipe/sharp-pipe.pipe.ts
+++ src/images/pipes/sharp-pipe/sharp-pipe.pipe.ts
@@ -1,4 +1,4 @@
-import { Injectable, PipeTransform } from '@nestjs/common';
+import { ArgumentMetadata, Injectable, PipeTransform } from '@nestjs/common';
 import * as sharp from 'sharp';
 
 @Injectable()
@@ -10,5 +10,24 @@ export class SharpPipe
       .resize(1200)
       .webp({ effort: 3 })
       .toBuffer();
+  }
+}
+
+@Injectable()
+export class SharpPipeMultiple
+  implements PipeTransform<Express.Multer.File[], Promise<Buffer[]>>
+{
+  async transform(images: Express.Multer.File[]): Promise<Buffer[]> {
+    const sharpPromises = images.map((img) => {
+      return sharp(img.buffer).resize(1200).webp({ effort: 3 }).toBuffer();
+    });
+    try {
+      const buffers = await Promise.all(sharpPromises);
+
+      return buffers;
+    } catch (e: any) {
+      console.error(e);
+      throw new Error('Error al procesar las imagenes');
+    }
   }
 }
