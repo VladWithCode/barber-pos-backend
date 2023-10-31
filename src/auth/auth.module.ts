fb845f83c9d77bbed3d4f0041331eac5812f7b7e
vladwithcode <vladwithcode@gmail.com>
2023-10-31 16:34:51 -0600

    Change sign validity time

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -13,7 +13,7 @@ import { JwtStrategy } from './strategies/jwt.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
-      signOptions: { expiresIn: '8h' },
+      signOptions: { expiresIn: '6h' },
     }),
   ],
   controllers: [AuthController],
